00018ce24859109c589d3711b618c7bfafc85b27
Andrey <stolbov.andrey2006@gmail.com>
2026-05-25 10:09:18 +0500

    Add Scroll

--- robots/src/gui/LogWindow.java
+++ robots/src/gui/LogWindow.java
@@ -3,6 +3,8 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.TextArea;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 import java.util.List;
@@ -10,6 +12,7 @@ import java.util.ResourceBundle;
 
 import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
 
 import log.LogChangeListener;
 import log.LogEntry;
@@ -20,6 +23,7 @@ public class LogWindow extends JInternalFrame implements LogChangeListener, Loca
   private LogWindowSource m_logSource;
   private TextArea m_logContent;
   private int scrollOffset;
+  private JScrollBar scrollBar;
 
   public LogWindow(LogWindowSource logSource) {
     super("", true, true, true, true);
@@ -29,8 +33,18 @@ public class LogWindow extends JInternalFrame implements LogChangeListener, Loca
     m_logContent.setSize(200, 500);
     scrollOffset = 0;
 
+    scrollBar = new JScrollBar(JScrollBar.VERTICAL);
+    scrollBar.addAdjustmentListener(new AdjustmentListener() {
+      @Override
+      public void adjustmentValueChanged(AdjustmentEvent e) {
+          scrollOffset = e.getValue();
+          updateLogContent();
+      }
+    });
+
     JPanel panel = new JPanel(new BorderLayout());
     panel.add(m_logContent, BorderLayout.CENTER);
+    panel.add(scrollBar, BorderLayout.EAST);
     getContentPane().add(panel);
     pack();
 
@@ -44,12 +58,22 @@ public class LogWindow extends JInternalFrame implements LogChangeListener, Loca
         } else {
           scrollOffset = Math.max(scrollOffset - 1, 0);
         }
+        scrollBar.setValue(scrollOffset);
         updateLogContent();
         e.consume();
       }
     });
 
     updateLogContent();
+  }
+
+  private void updateScrollBar() {
+    int total = m_logSource.size();
+    int maxOffset = Math.max(0, total - VISIBLE_COUNT);
+    scrollBar.setMinimum(0);
+    scrollBar.setMaximum(maxOffset + VISIBLE_COUNT);
+    scrollBar.setVisibleAmount(VISIBLE_COUNT);
+    scrollBar.setValue(Math.min(scrollOffset, maxOffset));
   }
 
   private void updateLogContent() {
@@ -64,6 +88,7 @@ public class LogWindow extends JInternalFrame implements LogChangeListener, Loca
     }
     m_logContent.setText(content.toString());
     m_logContent.invalidate();
+    updateScrollBar();
   }
 
   @Override
